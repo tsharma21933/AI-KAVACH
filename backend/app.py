4f5747ea6153ccb34bda5c2f17406bc8128c6de1
garimanayar <Nayargarima2004@gmail.com>
2026-04-08 00:53:06 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -2,15 +2,30 @@ from flask import Flask, request, render_template
 import joblib
 import numpy as np
 import os
+import gdown
 
 app = Flask(__name__,
             template_folder="templates",
             static_folder="static")
 
-# Paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 MODEL_PATH = os.path.join(BASE_DIR, "..", "model", "rul_model.pkl")
 SCALER_PATH = os.path.join(BASE_DIR, "..", "model", "scaler.pkl")
+
+# Google Drive File IDs
+MODEL_ID = "1Y4oLNpd2c2EysWpV-Bd2EKKdSzULS-qb"
+SCALER_ID = "1A7XoVTwKn0QL0cuiQWBZedaDqEA5MW42"
+
+# Download model if not present
+if not os.path.exists(MODEL_PATH):
+    os.makedirs(os.path.join(BASE_DIR, "..", "model"), exist_ok=True)
+    url = f"https://drive.google.com/uc?id={MODEL_ID}"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+if not os.path.exists(SCALER_PATH):
+    url = f"https://drive.google.com/uc?id={SCALER_ID}"
+    gdown.download(url, SCALER_PATH, quiet=False)
 
 # Load model
 model = joblib.load(MODEL_PATH)
